94697e5e33f78f44b43de1ff1feb9806e78d4424
poisonhs <baililovely@qq.com>
2026-07-24 02:55:14 +0000

    fix: IPA CFBundleExecutable 与主二进制同名，修复 Parse Error 303

--- build/ios/main.m
+++ build/ios/main.m
@@ -1,6 +1,13 @@
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 
+@interface RootViewController : UIViewController
+@end
+
+@implementation RootViewController
+- (BOOL)prefersStatusBarHidden { return NO; }
+@end
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) WKWebView *webView;
@@ -12,18 +19,27 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
 
-    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    self.webView = [[WKWebView alloc] initWithFrame:self.window.bounds configuration:config];
-    self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [self.window addSubview:self.webView];
+    RootViewController *root = [[RootViewController alloc] init];
+    root.view.backgroundColor = [UIColor whiteColor];
 
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html" inDirectory:@"www"];
-    if (path) {
-        NSURL *url = [NSURL fileURLWithPath:path];
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    config.allowsInlineMediaPlayback = YES;
+    self.webView = [[WKWebView alloc] initWithFrame:root.view.bounds configuration:config];
+    self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [root.view addSubview:self.webView];
+
+    // 优先加载本地 www/index.html；没有则加载远程页
+    NSString *local = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html" inDirectory:@"www"];
+    if (local) {
+        NSURL *url = [NSURL fileURLWithPath:local];
         NSURL *dir = [url URLByDeletingLastPathComponent];
         [self.webView loadFileURL:url allowingReadAccessToURL:dir];
+    } else {
+        NSURL *url = [NSURL URLWithString:@"https://app.hdraw.de"];
+        [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
     }
 
+    self.window.rootViewController = root;
     [self.window makeKeyAndVisible];
     return YES;
 }
